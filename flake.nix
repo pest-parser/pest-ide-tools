85ad16b4493b5c87fd9c990007b5249e356159f1
Jamalam <james@jamalam.tech>
2026-03-03 10:23:08 +0000

    chore: simplify nix setup (#206)

--- flake.nix
+++ flake.nix
@@ -1,71 +1,34 @@
 {
   inputs = {
-    utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      rust-overlay,
-      utils,
-    }:
-
-    utils.lib.eachDefaultSystem (
-      system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            rust-overlay.overlays.default
-          ];
         };
-        rustToolchain = (
-          pkgs.rust-bin.stable.latest.default.override {
-            extensions = [
-              "rust-src"
-              "rust-analyzer"
-            ];
-          }
-        );
-
       in
-      {
-        packages = rec {
-          pest-ide-tools =
-            (pkgs.makeRustPlatform {
-              rustc = rustToolchain;
-              cargo = rustToolchain;
-            }).buildRustPackage
-              rec {
-                name = "pest-langage-server";
-                src = pkgs.lib.cleanSource ./.;
-                cargoLock = {
-                  lockFile = ./Cargo.lock;
-                  allowBuiltinFetchGit = true;
-                };
+        with pkgs; {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              rustPackages.clippy
+              go-task
+              nodejs
+              pnpm
+            ];
 
-                meta.mainProgram = name;
-              };
-          default = pest-ide-tools;
-        };
-
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [
-            (lib.hiPrio rust-bin.nightly.latest.rustfmt)
-            go-task
-            nodejs
-            prettier
-            rustToolchain
-            typescript-language-server
-          ];
-        };
-      }
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+        }
     );
-
 }
